0be05daaf42d0a165a1760c69cfc1d94c9181670
Louise <lkosborne@live.com>
2023-07-31 14:50:16 +0100

    cleaning up formatting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const app = require('./src/app.js');
 
-const APP_PORT = process.env.PORT || 4000;
+const APP_PORT = process.env.PORT || 3000;
 
 app.listen(APP_PORT, () => {
     console.log(`App is listening on port ${APP_PORT}`)
